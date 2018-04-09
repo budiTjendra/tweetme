eb677853916fe148da363476a28f51761624ec9d
budiTjendra <buc3t@yahoo.com>
2018-04-09 11:56:54 +0800

    code refactoring

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import OAuthReducers from './OAuthReducers';
 import TweetReducers from './TweetReducers';
-//import LoadingReducers from './LoadingReducers';
 
 export default combineReducers ({
   oauth: OAuthReducers,
   tweet: TweetReducers
-  //loading: LoadingReducers
 });
